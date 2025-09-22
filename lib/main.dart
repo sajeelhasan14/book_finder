79e73cbe31dac72212fe1d2d35b74c9465e8acd8
sajeelhasan14 <sajeel.hasan14@gmail.com>
2025-09-22 05:33:09 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:book_finder/providers/auth_provider.dart';
+import 'package:book_finder/providers/author_provider.dart';
+import 'package:book_finder/providers/editions_provider.dart';
+import 'package:book_finder/providers/favorite_provider.dart';
 import 'package:book_finder/providers/search_provider.dart';
+import 'package:book_finder/providers/settings_provider.dart';
 import 'package:book_finder/providers/signup_screen_provider.dart';
+import 'package:book_finder/providers/subject_provider.dart';
 import 'package:book_finder/providers/work_detail_provider.dart';
-import 'package:book_finder/screens/auth/signup_screen.dart';
+
+import 'package:book_finder/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -21,18 +27,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider<AuthProvider>(create: (_) => AuthProvider()),
-        ChangeNotifierProvider<SignupScreenProvider>(create: (_) => SignupScreenProvider(),),
-        ChangeNotifierProvider<SearchProvider>(create: (_) => SearchProvider()),
-        // WorkDetailProvider is usually created per screen,
-        // but you can also provide globally if needed
-        ChangeNotifierProvider<WorkDetailProvider>(
-          create: (_) => WorkDetailProvider(),
-        ),
+         ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => AuthorProvider()),
+        ChangeNotifierProvider(create: (_) => EditionsProvider()),
+        ChangeNotifierProvider(create: (_) => FavoritesProvider()),
+        ChangeNotifierProvider(create: (_) => SearchProvider()),
+        ChangeNotifierProvider(create: (_) => SettingsProvider()),
+        ChangeNotifierProvider(create: (_) => SignupScreenProvider()),
+        ChangeNotifierProvider(create: (_) => SubjectProvider()),
+        ChangeNotifierProvider(create: (_) => WorkDetailProvider()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: const SignUpScreen(),
+        home: const HomeScreen(),
       ),
     );
   }
